b320dd2810321c3183b050f117dbb5be7cb5989c
rademihajlovic <mihajlovicrade92@gmail.com>
2024-09-06 23:57:32 +0200

    changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'https://atel-control.rademihajlovic.pro/api/v1/';
+export const API_BASE_URL = 'http://localhost:3000/api/v1/';
 
 const api = axios.create({
-	baseURL: API_BASE_URL,
+	baseURL: `${API_BASE_URL}`,
 	headers: {
 		'Content-Type': 'application/json',
 	},
--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { API_BASE_URL } from '../../api/api';
 
 const ProductCard = function ({ product }) {
+	const imageUrl = `${API_BASE_URL}images/${product.images[0]}`;
 	return (
 		<Link to={`/products/${product._id}`} className="product d-flex flex-column m-1 p-0 position-relative" key={product._id}>
-			<img src={`http://localhost:3000/api/images/${product.images[0]}`} alt={product.name} />
+			<img src={imageUrl} alt={product.name} />
 			<div className="position-absolute bottom-0 col-12 d-flex flex-column justify-content-center aligh-items-center text-dark text-center bg-dark opacity-50">
 				<p className="title mt-2">{product.name}</p>
 				<p className="price fw-bolder my-1">
